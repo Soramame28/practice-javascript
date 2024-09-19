fbe5f9e12fd3a3c6032afe7d44714619f3f5ca26
Soramame28 <kakerufujita912@gmail.com>
2024-09-19 05:19:42 +0000

    9_19JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("h1").textContent="自分の名前"
+document.querySelector("h2").textContent="fuji"
+
+//documentとは！！
+/* htmlファイルで書かれているコードを一つのオブジェクトとしてみなす */
+
+//DOM(ドム)
+/* ドキュメントオブジェクトモデル　（Document Object Model）の略で、
+webページの要素やコンテンツなどをツリー構造で表現したデータモデル */
+
+//上記コードの意味
+//document.querySelector("h1").textContent="自分の名前"
+
+//ドキュメントの中の（"h1"）タグを探してね
+//document.querySelector("h1")
