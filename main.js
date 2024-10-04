f257c5360c027722d3da22b57f60802ba29e910f
Soramame28 <kakerufujita912@gmail.com>
2024-10-04 06:04:31 +0000

    10_4

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-//クリックされたら動作する
-
 function errorInput(){
-    console.log(document.getElementById("month").value)
-    console.log(document.getElementById("day").value)
-    
     const month = document.getElementById("month").value;
     const day = document.getElementById("day").value;
-
+   if(month<1 || month>12){
+        alert("適切な値ではありません");
+        return
+   }
     console.log("month",month,"day",day)
 }
-//document.getElementById("month")はタグを確認するだけ
-//valueはそのタグの値を示す
-
-/*
-javaScriptのプログラムとHTMLファイルはどこで対応しているか
-タグについているIDで紐づく
-タグの中にある付加情報のことを属性という
-console.log()はC言語でいうprintf();です
-*/
