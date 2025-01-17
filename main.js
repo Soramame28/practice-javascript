6058627f00e3bb2b2db621f5ed355584835cc9fd
Soramame28 <kakerufujita912@gmail.com>
2025-01-17 06:18:51 +0000

    数当てゲームの追加

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function ClickCounterGame(){
     let count = 0;
-
-    const gameContainer = document.getElementById("game-container")
     
+    const gameContainer = document.getElementById("game-container")
+   
     let button1 = document.createElement("button");
     button1.textContent = "+1ボタン";
 
@@ -12,20 +12,30 @@ function ClickCounterGame(){
     let button3 = document.createElement("button");
     button3.textContent = "リセット";
 
+    let sa
+
     let counter = document.createElement("p");
     counter.textContent = count;
-
     
     button1.addEventListener("click",function(){
-        count++;
-        counter.textContent = count;
+        if(count < 100){
+            count++;
+            counter.textContent = count;
+        }
     })
 
     button2.addEventListener("click",function(){
-        count = count + 10;
-        counter.textContent = count;
+         if(count < 100){
+             count = count + 10;
+             counter.textContent = count;
+         }if(count > 100){
+             sa = count -100;
+             count = count - sa;
+             counter.textContent = count;
+         }
+        
     })
-
+    
     button3.addEventListener("click",function(){
         count = 0;
         counter.textContent = count;
@@ -35,5 +45,26 @@ function ClickCounterGame(){
     gameContainer.appendChild(button2);
     gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
+    
 }
 ClickCounterGame();
+
+function startNumberGuessGame(){
+    
+    const gameContainer = document.getElementById("game-container")
+    
+    const randomNumber = Math.floor(Math.random()*100) + 1;
+    
+    let message = document.createElement("p");
+    message.textContent = randomNumber;
+    
+    let input = document.createElement("input");
+    
+    input.type = "number";
+    
+    input.placeholder = "好きな数字を入力してください (1 ~ 100)";
+    
+    gameContainer.appendChild(message);
+    gameContainer.appendChild(input);
+}
+startNumberGuessGame();
