e888e8485b0d8ce7baa70acabda01136c5a2d684
Soramame28 <kakerufujita912@gmail.com>
2025-01-24 06:15:15 +0000

    判定ボタンの追加

--- main.js
+++ main.js
@@ -1,55 +1,61 @@
-//  function ClickCounterGame(){
-//     let count = 0;
+ function ClickCounterGame(){
+    let count = 0;
     
-//     const gameContainer = document.getElementById("game-container")
+    const gameContainer = document.getElementById("game-container")
    
-//     let button1 = document.createElement("button");
-//     button1.textContent = "+1ボタン";
+    let button1 = document.createElement("button");
+    button1.textContent = "+1ボタン";
 
-//     let button2 = document.createElement("button");
-//     button2.textContent = "+10ボタン";
+    let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
 
-//     let button3 = document.createElement("button");
-//     button3.textContent = "リセット";
+    let button3 = document.createElement("button");
+    button3.textContent = "リセット";
 
-//     let sa
+    let sa
 
-//     let counter = document.createElement("p");
-//     counter.textContent = count;
+    let counter = document.createElement("p");
+    counter.textContent = count;
     
-//     button1.addEventListener("click",function(){
-//         if(count < 100){
-//             count++;
-//             counter.textContent = count;
-//         }
-//     })
+    button1.addEventListener("click",function(){
+        if(count < 100){
+            count++;
+            counter.textContent = count;
+        }
+    })
 
-//     button2.addEventListener("click",function(){
-//          if(count < 100){
-//              count = count + 10;
-//              counter.textContent = count;
-//          }if(count > 100){
-//              sa = count -100;
-//              count = count - sa;
-//              counter.textContent = count;
-//          }
+    button2.addEventListener("click",function(){
+         if(count < 100){
+             count = count + 10;
+             counter.textContent = count;
+         }if(count > 100){
+             sa = count -100;
+             count = count - sa;
+             counter.textContent = count;
+         }
         
-//     })
+    })
     
-//     button3.addEventListener("click",function(){
-//         count = 0;
-//         counter.textContent = count;
-//     })
+    button3.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
 
-//     gameContainer.appendChild(button1);
-//     gameContainer.appendChild(button2);
-//     gameContainer.appendChild(button3);
-//     gameContainer.appendChild(counter);
+    gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(button3);
+    gameContainer.appendChild(counter);
     
-//  }
-// ClickCounterGame();
+ }
+ClickCounterGame();
 
 function startNumberGuessGame(){
+    
+    let count = 0;
+    
+    
+    
+    
     
     const gameContainer = document.getElementById("game-container")
     
@@ -71,21 +77,21 @@ function startNumberGuessGame(){
     button.textContent = "確認"
 
     button.addEventListener("click",function(){
-        let inzery = parseInt(input.value)
-        if (randomNumber == inzery){
+        const val = parseInt(input.value)
+        if (randomNumber == val){
             message.textContent = "正解"
         
-        }else if(randomNumber < inzery){
+        }else if(randomNumber < val){
             
             message.textContent = "大きい"
-        }else if(randomNumber > inzery){
+        }else if(randomNumber > val){
             
             message.textContent = "小さい"
         }else{
             
             message.textContent = "はよやれ"
         }
-
+// message.textContent = `${Math.abs(randomNumber - inzery) <= 5 ? "惜しい！": "" }${randomNumber > inzery?"大きい": "小さい"}` by Noritake
     })
     gameContainer.appendChild(button);
 
@@ -97,5 +103,7 @@ function startNumberGuessGame(){
     })
 
     gameContainer.appendChild(button5)
+
+    
 }
 startNumberGuessGame();
