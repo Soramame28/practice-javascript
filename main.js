33d2fe9e844d12b80002e529c216adae7aca274d
Soramame28 <kakerufujita912@gmail.com>
2025-01-31 08:00:58 +0000

    計算機

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ gameSelector.addEventListener('change',function(){
         case 'rps':
 
             break;
-    
+        case 'kei':
+            keisanki();
+            break;
     }
 })
 
@@ -130,4 +132,59 @@ function startNumberGuessGame(){
     countDisplay.textContent = `試行回数： ${count2}`;
     gameContainer.appendChild(countDisplay);
     
+}
+
+
+function keisanki(){
+    let ka = 0;
+    
+    const gameContainer = document.getElementById("game-container")
+
+    let input1 = document.createElement("input")
+    input1.placeholder = "入力１（半角）"
+
+    let input2 = document.createElement("input")
+    input2.placeholder = "入力２（半角）"
+
+    let purasu = document.createElement("button")
+    purasu.textContent = "＋"
+
+    let kekka = document.createElement("p")
+    kekka.textContent = ka
+
+    purasu.addEventListener("click",function(){
+        ka = 0;
+        let a = parseInt(input1.value)
+        let b = parseInt(input2.value)
+        if(a >= 0 && b >= 0){
+            ka = a + b;
+
+        }else if (a >= 0){
+             ka = a;
+            
+        }else if (b >= 0){
+             ka = b;
+             
+        }else if (a < 0 && b < 0){
+            ka = a + b
+        
+        }else if (a < 0 && b < 0){
+            ka = a + b
+        
+        }else if (a < 0){
+            ka = a;
+           
+       }else if (b < 0){
+        ka = b;
+        
+   }
+        
+        kekka.textContent = ka
+    })
+
+
+    gameContainer.appendChild(kekka)
+    gameContainer.appendChild(input1)
+    gameContainer.appendChild(input2)
+    gameContainer.appendChild(purasu)
 }
