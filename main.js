513786c99cf9e74120b23c26119f2901f57cc050
Soramame28 <kakerufujita912@gmail.com>
2025-02-12 07:58:42 +0000

    計算機

--- main.js
+++ main.js
@@ -149,14 +149,23 @@ function keisanki(){
     let purasu = document.createElement("button")
     purasu.textContent = "＋"
 
+    let mainasu = document.createElement("button")
+    mainasu.textContent = "－"
+
+    let jou = document.createElement("button")
+    jou.textContent = "✖"
+
+    let jo = document.createElement("button")
+    jo.textContent = "÷"
+
     let kekka = document.createElement("p")
     kekka.textContent = ka
 
     purasu.addEventListener("click",function(){
         ka = 0;
-        let a = parseInt(input1.value)
-        let b = parseInt(input2.value)
-        if(a >= 0 && b >= 0){
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        if(a >= 0 && b >= 0 || a < 0 && b >= 0 || a >= 0 && b < 0 || a < 0 && b < 0){
             ka = a + b;
 
         }else if (a >= 0){
@@ -165,12 +174,6 @@ function keisanki(){
         }else if (b >= 0){
              ka = b;
              
-        }else if (a < 0 && b < 0){
-            ka = a + b
-        
-        }else if (a < 0 && b < 0){
-            ka = a + b
-        
         }else if (a < 0){
             ka = a;
            
@@ -182,9 +185,97 @@ function keisanki(){
         kekka.textContent = ka
     })
 
+    mainasu.addEventListener("click",function(){
+        ka = 0;
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        if(a >= 0 && b >= 0 || a < 0 && b >= 0 || a >= 0 && b < 0 || a < 0 && b < 0){
+            ka = a - b;
+
+        }else if (a >= 0){
+             ka = a;
+            
+        }else if (b >= 0){
+             ka = b;
+             
+        }else if (a < 0){
+            ka = a;
+           
+       }else if (b < 0){
+        ka = b;
+        
+   }
+        
+        kekka.textContent = ka
+    })
+
+    purasu.addEventListener("click",function(){
+        ka = 0;
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        if(a >= 0 && b >= 0 || a < 0 && b >= 0 || a >= 0 && b < 0 || a < 0 && b < 0){
+            ka = a + b;
+
+        }else if (a >= 0){
+             ka = a;
+            
+        }else if (b >= 0){
+             ka = b;
+             
+        }else if (a < 0){
+            ka = a;
+           
+       }else if (b < 0){
+        ka = b;
+        
+   }
+        
+        kekka.textContent = ka
+    })
+
+    jou.addEventListener("click",function(){
+        ka = 0;
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        if(a >= 0 && b >= 0 || a < 0 && b >= 0 || a >= 0 && b < 0 || a < 0 && b < 0){
+            ka = a * b;
+
+        }else if (a >= 0){
+             ka = a * 0;
+            
+        }else if (b >= 0){
+             ka = b * 0;
+             
+        }else if (a < 0){
+            ka = a * 0;
+           
+       }else if (b < 0){
+        ka = b * 0;
+        
+   }
+        
+        kekka.textContent = ka
+    })
+
+    jo.addEventListener("click",function(){
+        ka = 0;
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        if(b != 0){
+            ka = a / b;
+            kekka.textContent = ka
+        }else{
+        kekka.textContent = "割れない"
+       }
+        
+        
+    })
 
     gameContainer.appendChild(kekka)
     gameContainer.appendChild(input1)
     gameContainer.appendChild(input2)
     gameContainer.appendChild(purasu)
+    gameContainer.appendChild(mainasu)
+    gameContainer.appendChild(jou)
+    gameContainer.appendChild(jo)
 }
